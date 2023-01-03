13a35c286ec625d27287351515e2f6e248f6e878
MccGrey <chuxzzie01@yahoo.com>
2023-01-03 17:03:11 +0100

    create expenseitem component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Lets get started</h1>
+      <ExpenseItem />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,9 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h1>Lets get started</h1>
+    </div>
+  );
+}
+
+export default ExpenseItem;
