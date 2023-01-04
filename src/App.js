387b35d41ab4b5dcaff5f79c9c06a1b3a5f1abc6
MccGrey <chuxzzie01@yahoo.com>
2023-01-04 08:21:41 +0100

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
 import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: "el",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2022, 7, 23),
+    },
+
+    {
+      id: "e1",
+      title: "Provision",
+      amount: 65.12,
+      date: new Date(2022, 4, 3),
+    },
+
+    {
+      id: "e2",
+      title: "Paperweights",
+      amount: 4.65,
+      date: new Date(2022, 5, 21),
+    },
+
+    {
+      id: "e3",
+      title: "LED TY",
+      amount: 124.12,
+      date: new Date(2022, 8, 13),
+    },
+
+    {
+      id: "e4",
+      title: "mac Book",
+      amount: 543.22,
+      date: new Date(2022, 7, 23),
+    },
+
+    {
+      id: "e5",
+      title: "Swim Pool",
+      amount: 1984.12,
+      date: new Date(2022, 2, 24),
+    },
+  ];
   return (
     <div>
       <h1>Lets get started</h1>
-      <Expenses />
+      <Expenses items={expenses} />
     </div>
   );
 }
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,81 +1,37 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
-function Expenses() {
-  const expenses = [
-    {
-      id: "el",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2022, 7, 23),
-    },
-
-    {
-      id: "e1",
-      title: "Provision",
-      amount: 65.12,
-      date: new Date(2022, 4, 3),
-    },
-
-    {
-      id: "e2",
-      title: "Paperweights",
-      amount: 4.65,
-      date: new Date(2022, 5, 21),
-    },
-
-    {
-      id: "e3",
-      title: "LED TY",
-      amount: 124.12,
-      date: new Date(2022, 8, 13),
-    },
-
-    {
-      id: "e4",
-      title: "mac Book",
-      amount: 543.22,
-      date: new Date(2022, 7, 23),
-    },
-
-    {
-      id: "e5",
-      title: "Swim Pool",
-      amount: 1984.12,
-      date: new Date(2022, 2, 24),
-    },
-  ];
-
+function Expenses(props) {
   return (
     <div className="expenses">
       <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
       />
 
       <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
       />
 
       <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
       />
 
       <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
       />
 
       <ExpenseItem
-        title={expenses[4].title}
-        amount={expenses[4].amount}
-        date={expenses[4].date}
+        title={props.items[4].title}
+        amount={props.items[4].amount}
+        date={props.items[4].date}
       />
     </div>
   );
