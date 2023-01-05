6a08416daf9f6165dab75eddc04b326eefc66e8e
MccGrey <chuxzzie01@yahoo.com>
2023-01-05 09:13:23 +0100

    update the list with data input

--- src/App.js
+++ src/App.js
@@ -1,54 +1,58 @@
+import { useState } from "react";
 import Expenses from "../src/components/expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_DATA = [
+  {
+    id: "el",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2022, 7, 23),
+  },
+
+  {
+    id: "e1",
+    title: "Provision",
+    amount: 65.12,
+    date: new Date(2022, 4, 3),
+  },
+
+  {
+    id: "e2",
+    title: "Paperweights",
+    amount: 4.65,
+    date: new Date(2022, 5, 21),
+  },
+
+  {
+    id: "e3",
+    title: "LED TY",
+    amount: 124.12,
+    date: new Date(2022, 8, 13),
+  },
+
+  {
+    id: "e4",
+    title: "mac Book",
+    amount: 543.22,
+    date: new Date(2022, 7, 23),
+  },
+
+  {
+    id: "e5",
+    title: "Swim Pool",
+    amount: 1984.12,
+    date: new Date(2022, 2, 24),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "el",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2022, 7, 23),
-    },
-
-    {
-      id: "e1",
-      title: "Provision",
-      amount: 65.12,
-      date: new Date(2022, 4, 3),
-    },
-
-    {
-      id: "e2",
-      title: "Paperweights",
-      amount: 4.65,
-      date: new Date(2022, 5, 21),
-    },
-
-    {
-      id: "e3",
-      title: "LED TY",
-      amount: 124.12,
-      date: new Date(2022, 8, 13),
-    },
-
-    {
-      id: "e4",
-      title: "mac Book",
-      amount: 543.22,
-      date: new Date(2022, 7, 23),
-    },
-
-    {
-      id: "e5",
-      title: "Swim Pool",
-      amount: 1984.12,
-      date: new Date(2022, 2, 24),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_DATA);
 
   const addExpenseHandler = (expense) => {
-    console.log("app");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
